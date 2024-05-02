8498f0d9ee7068b8f605874ccc499ec1a733d03a
Sistemas <sistemas@ferrosa.mx>
2024-05-01 19:30:27 -0600

    student module

--- module_git/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Module GIT', 
-    'summary': 'A Git test module ing chris padilla',
-    'description': '''
-        This module is to see the git flow of modules in branch
-    ''',
-    'version': '17.0.1', 
-    'installable': True,
-}
